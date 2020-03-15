a1b3c8f02c736c3b838de240bba5b31a99bcd99d
Sonali <Sonali11091996@gmail.com>
2020-03-15 19:08:28 +0530

    First Commit

---
+++ sampleOne/src/com/sonali/Main.java
@@ -0,0 +1,22 @@
+package com.sonali;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+ public class Main  {
+     @Override
+     public String toString() {
+         return "Foo";
+     }
+
+     public static void main(String[] args) {
+        List l=new ArrayList<Integer>();
+        l.add(1);
+        l.add(2);l.add(3);
+
+     }
+ }
