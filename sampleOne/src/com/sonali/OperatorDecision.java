ff96fb1f00ee9c80212570bd464cc0652718f8e5
Sonali <Sonali11091996@gmail.com>
2020-03-16 15:08:39 +0530

    OperartorDecision

---
+++ sampleOne/src/com/sonali/OperatorDecision.java
@@ -0,0 +1,14 @@
+package com.sonali;
+
+public class OperatorDecision {
+    public static void main(String[] args) {
+        int i;int j;
+        for(i=1,j=3;i<=j;i=i+2,j++){
+            i++;
+        }
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
+
+
